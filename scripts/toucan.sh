48646cd796cb824a6086cd8cfd18610ad2b62afe
Tibor Bodecs <mail.tib@gmail.com>
2025-06-27 14:55:25 +0200

    fix dir

--- scripts/toucan.sh
+++ scripts/toucan.sh
@@ -5,4 +5,8 @@ log() { printf -- "** %s\n" "$*" >&2; }
 error() { printf -- "** ERROR: %s\n" "$*" >&2; }
 fatal() { error "$@"; exit 1; }
 
-docker run --rm -v $(pwd):/app/site --entrypoint toucan toucansites/toucan generate /app/site/src /app/site/dist
+DIR="$(pwd)"
+
+echo $DIR;
+
+docker run --rm -v "$(pwd)":/app/site --entrypoint toucan toucansites/toucan generate /app/site/src /app/site/dist
